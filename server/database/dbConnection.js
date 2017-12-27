242ae8a9df9bc0c5824e89a39b9bd06f05235496
Santiago Iraola <iraola.santi@gmail.com>
2017-12-27 13:11:46 -0800

    refactor db controller to use sequelize

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -1,4 +1,9 @@
-mysql = require('mysql');
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('movie_list', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 exports.connection = mysql.createConnection({
   user: 'root',
@@ -16,3 +21,26 @@ exports.connectDB = () => {
 }
 
 exports.connectDB();
+
+
+exports.Movie = sequelize.define('movie', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  img_source: {
+    type: Sequelize.STRING
+  },
+  to_watch: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+});
--- server/database/dbController.js
+++ server/database/dbController.js
@@ -1,41 +1,28 @@
 var db = require('./dbConnection');
-var Promsie = require('bluebird');
+var Promise = require('bluebird');
 
 exports.getMoviesFromDB = () => {
-  return new Promise((resolve, reject) => {
-    let q = 'SELECT * FROM movies';
-    db.connection.query(q, (err, result) => {
-      if(err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
+
+  return db.Movie.sync().then(() => db.Movie.findAll());
 };
 
 exports.saveMovieInDB = (movie) => {
-  return new Promise((resolve, reject) => {
-    let q = `INSERT INTO movies (id, title, description, img_source, to_watch) VALUES (${movie.id}, "${movie.name}", "${movie.text}", "${movie.imgSource}", ${movie.toWatch})`;
-    db.connection.query(q, (err, result) => {
-      if(err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
+  return db.Movie.create({
+    id: movie.id,
+    title: movie.name,
+    description: movie.text,
+    img_source: movie.imgSource,
+    to_watch: movie.toWatch
   });
 }
 
 exports.updateMovie = (movie) => {
-  return new Promise((resolve, reject) => {
-    let q = `UPDATE movies SET to_watch = ${movie.toWatch} WHERE ${movie.id} = id`
-    db.connection.query(q, (err, result) => {
-      if(err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
+
+  return db.Movie.update({
+    to_watch: movie.toWatch
+  }, {
+    where: {
+      id: movie.id
+    }
   });
 };
